8c006554c12f0e2036f90b306605ed1ecd5a2a9e
arshivhare <arshivhare@gmail.com>
2020-04-12 20:06:36 -0400

    Updates

---
+++ precache-manifest.29a630fc0d9050f8764a062571db2ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e6942b6c2a67494742d14ba10021b2",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "77dff298ef8e5d8ff8f7",
+    "url": "/to-do-list/static/css/main.3bff57ed.chunk.css"
+  },
+  {
+    "revision": "499385934aac86a93530",
+    "url": "/to-do-list/static/js/2.5ed69d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.5ed69d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77dff298ef8e5d8ff8f7",
+    "url": "/to-do-list/static/js/main.c7ded3c5.chunk.js"
+  },
+  {
+    "revision": "01bcd2c198c66dc062a3",
+    "url": "/to-do-list/static/js/runtime-main.bc02af80.js"
+  }
+]);
